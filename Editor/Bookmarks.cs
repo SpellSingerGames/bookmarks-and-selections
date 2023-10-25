11971c1e99cdd4bebb590f18f292a4169077af38
sinedsem <sinedsem@gmail.com>
2023-10-25 22:45:24 +0300

    initial commit

---
+++ Editor/Bookmarks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpellSinger.BookmarksAndSelections
+{
+    public class Bookmarks : ScriptableObject
+    {
+        [SerializeField]
+        private List<Object> bookmarks = new();
+            
+        public List<Object> List
+        {
+            get => bookmarks;
+            set => bookmarks = value;
+        }
+    }
+}
